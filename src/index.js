65ac9fd8a12af9a1baca86f4530e221e74ddc189
Stephen Malek <malek.stephen44@gmail.com>
2018-10-10 11:46:42 -0600

    add new word

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import $ from 'jquery'
-var baseUrl = 'https://wordwatch-api.herokuapp.com'
+
 $(document).ready(() => {
   loadWords()
 })
 
+var baseUrl = 'https://wordwatch-api.herokuapp.com'
 
+
+
+function addNewWord() {
+  var url = `${baseUrl}/api/v1/words`
+  var newWord = $('textarea').val()
+  var payload = { word: { value: newWord } }
+  var stringPayload = JSON.stringify(payload)
+  fetch(url, {
+    headers: {'Content-Type': 'application/json'},
+    method: 'POST',
+    body: stringPayload
+  })
+  $('textarea').val("")
+  loadWords()
+}
 
 function loadWords() {
   fetch(`${baseUrl}/api/v1/top_word`)
     .then(response => response.json())
     .then(function(data) {
       var word = data["word"]
-      $('article.word-count').append(`${Object.keys(word)[0]}: ${Object.values(word)[0]}`)
-
+      var field = $('article.word-count')
+      field.html("")
+      field.append(`${Object.keys(word)[0]}: ${Object.values(word)[0]}`)
     })
 }
+
+$('button').on('click', addNewWord)
