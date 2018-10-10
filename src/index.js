e82159d0b4b5b7fc839deca732820f4bd9ca2e7a
Stephen Malek <malek.stephen44@gmail.com>
2018-10-10 10:14:05 -0600

    top word appears on the page with count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
-
+var baseUrl = 'https://wordwatch-api.herokuapp.com'
 $(document).ready(() => {
-  // have fun!
+  loadWords()
 })
+
+
+
+function loadWords() {
+  fetch(`${baseUrl}/api/v1/top_word`)
+    .then(response => response.json())
+    .then(function(data) {
+      var word = data["word"]
+      $('article.word-count').append(`${Object.keys(word)[0]}: ${Object.values(word)[0]}`)
+
+    })
+}
